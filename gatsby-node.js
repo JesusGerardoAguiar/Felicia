8249e01b32bf1e233d3ddbf46b0bc9d058eb30e1
Jesus Aguiar <aguiarf.j16@gmail.com>
2020-03-29 03:24:44 -0300

    hotfix

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,23 +7,23 @@ exports.createPages = ({ graphql, actions }) => {
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
   return graphql(
     `
-      {
-        allMdx(
-          sort: { order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
+    {
+      allMdx(
+        sort: { fields: [frontmatter___activityTitle], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
             }
           }
         }
       }
+    }
     `
   ).then(result => {
     if (result.errors) {
@@ -32,21 +32,23 @@ exports.createPages = ({ graphql, actions }) => {
 
     // Create blog posts pages.
     const posts = result.data.allMdx.edges
-
-    posts.forEach((post, index) => {
-      const previous = index === posts.length - 1 ? null : posts[index + 1].node
-      const next = index === 0 ? null : posts[index - 1].node
-
-      createPage({
-        path: `blog${post.node.fields.slug}`,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
+    if(posts){
+      posts.forEach((post, index) => {
+        const previous = index === posts.length - 1 ? null : posts[index + 1].node
+        const next = index === 0 ? null : posts[index - 1].node
+  
+        createPage({
+          path: `blog${post.node.fields.slug}`,
+          component: blogPost,
+          context: {
+            slug: post.node.fields.slug,
+            previous,
+            next,
+          },
+        })
       })
-    })
+      return null;
+    }
 
     return null
   })
